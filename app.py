70730eda79ec6d365adfd65c0b7cb31e1166f24d
soumyabani <banisoumya3@gmail.com>
2025-12-06 11:33:32 +0000

    changes to application

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ import os
 
 from ocr import extract_text_from_image, extract_text_from_pdf
 
-API_URL = "https://eux38pbb50.execute-api.us-east-1.amazonaws.com/default"
+API_URL = "https://eux38pbb50.execute-api.us-east-1.amazonaws.com/default/carbonwise"
 
-st.title("EcoGuard – AI Carbon Footprint Analyzer")
+st.title("CarbonWise – AI Carbon Footprint Analyzer")
 
 uploaded = st.file_uploader("Upload Bill or Receipt", type=["pdf", "png", "jpg", "jpeg"])
 
