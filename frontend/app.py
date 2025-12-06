119cf966a452eb9e4dbabe2dd9747de9753aa49d
soumyabani <banisoumya3@gmail.com>
2025-12-06 10:27:11 +0000

    updated the app.py file

--- frontend/app.py
+++ frontend/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import requests
 import tempfile
 from PIL import Image
+import sys
 sys.path.append("../backend")
 from ocr import extract_text_from_image, extract_text_from_pdf
 
