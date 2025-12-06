30216865a6e6c704b0d4b66393a8c8085578d29c
soumyabani <banisoumya3@gmail.com>
2025-12-06 13:53:10 +0000

    updating application

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,16 +1,15 @@
 import json
-from ocr import extract_text_from_image, extract_text_from_pdf
 from extractor import extract_fields
 from carbon_calculator import compute_carbon
 
 def lambda_handler(event, context):
     try:
-        # body is a JSON string from API Gateway
         body = event.get("body") or "{}"
         if isinstance(body, str):
             body = json.loads(body)
 
         text = body.get("text", "")
+
         if not text:
             return {
                 "statusCode": 400,
@@ -33,7 +32,6 @@ def lambda_handler(event, context):
         }
 
     except Exception as e:
-        # for CloudWatch debugging
         print("Error in lambda_handler:", str(e))
         return {
             "statusCode": 500,
